6056dffdfb5e718f00fd635c8a77cae2d2660a33
Anh Tran <anh.tran@zalora.com>
2019-08-21 16:06:25 +0700

    develop commit

--- STRService/STRServiceExample/STRServiceExample/AppDelegate.swift
+++ STRService/STRServiceExample/STRServiceExample/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        //devlop commit
         strConfig(config: self)
         configure(dependency: CoreDependency())
         
